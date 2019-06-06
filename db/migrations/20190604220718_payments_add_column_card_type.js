5e4e8f1131bda6cb871397a0dc9d0c5eacce6e63
greg bruins <gregbruins@gmail.com>
2019-06-05 22:02:46 -0700

    changing column name from card_type to payment_type

--- db/migrations/20190604220718_payments_add_column_card_type.js
+++ db/migrations/20190604220718_payments_add_column_card_type.js
@@ -3,12 +3,12 @@ const CoreService = require('../../server/plugins/core/core.service');
 
 exports.up = function(knex, Promise) {
     return knex.schema.table(CoreService.DB_TABLES.payments, (t) => {
-        t.string('card_type').nullable();
+        t.string('payment_type').nullable();
     })
 };
 
 exports.down = function(knex, Promise) {
     return knex.schema.table(CoreService.DB_TABLES.payments, (t) => {
-        t.dropColumn('card_type');
+        t.dropColumn('payment_type');
     })
 };
