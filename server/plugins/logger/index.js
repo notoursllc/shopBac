3fbf4332843169661aa04cf6000bf1f8330463bf
greg bruins <gbruins@users.noreply.github.com>
2020-09-06 11:29:46 -0700

    feat(logdna-winston): LogDNA addon with Render
    allows me to remove the logdna-winston lib

--- server/plugins/logger/index.js
+++ server/plugins/logger/index.js
@@ -1,4 +1,3 @@
-const logdnaWinston = require('logdna-winston');
 const winston = require('winston');
 const bugsnag = require('@bugsnag/js');
 const isObject = require('lodash.isobject');
@@ -8,6 +7,7 @@ exports.plugin = {
     once: true,
     pkg: require('./package.json'),
     register: function (server, options) {
+
         // Bugsnag setup:
         const bugsnagClient = bugsnag({
             apiKey: process.env.BUG_SNAG_API_KEY,
@@ -21,61 +21,29 @@ exports.plugin = {
             }
         };
 
-        let logger = null;
-
         const prettyJson = winston.format.printf((info) => {
             if (isObject(info.meta)) {
-                info.meta = `- ${JSON.stringify(info.meta)}`
+                info.meta = `- ${JSON.stringify(info.meta)}`;
             }
             return `${info.timestamp} [${info.level}]: ${info.message} ${info.meta}`;
         });
 
-        // Log DNA setup:
-        if(process.env.NODE_ENV === 'production') {
-            logger = winston.createLogger({
-                format: winston.format.combine(
-                    winston.format.errors({ stack: true }),
-                    // This doesn't acutally format the results in LogDNA, except that it does cause
-                    // the 'meta' object to be stringified in the LogDNA UI, which is all I really want.
-                    // A 'prettiefied' meta object in LogDNA is kind of annoying read, I think.
-                    prettyJson
-                ),
-                transports: [
-                    new logdnaWinston({
-                        key: process.env.LOG_DNA_INGESTION_KEY,
-                        hostname: process.env.DOMAIN_NAME,
-                        // ip: ipAddress,
-                        // mac: macAddress,
-                        app: 'web',
-                        env: process.env.NODE_ENV,
-                        level: 'info', // Default to debug, maximum level of log, doc: https://github.com/winstonjs/winston#logging-levels
-                        index_meta: false, // Defaults to false, when true ensures meta object will be searchable
-                        handleExceptions: true,
-                        exitOnError: false
-                    })
-                ]
-            });
-        }
-        else {
-            logger = winston.createLogger({
-                format: winston.format.combine(
-                    winston.format.errors({ stack: true }),
-                    winston.format.colorize(),
-                    winston.format.timestamp(),
-                    winston.format.prettyPrint(),
-                    // winston.format.json(),
-                    // winston.format.splat(),
-                    // winston.format.simple(),
-                    // prettyJson
-                ),
-                transports: [
-                    new winston.transports.Console({
-                        level: process.env.NODE_ENV === 'test' ? 'error' : (process.env.LOG_LEVEL || 'info')
-                    })
-                ]
-            });
-        }
+        global.logger = winston.createLogger({
+            format: winston.format.combine(
+                winston.format.errors({ stack: true }),
+                winston.format.colorize(),
+                winston.format.timestamp(),
+                // This doesn't acutally format the results in LogDNA, except that it does cause
+                // the 'meta' object to be stringified in the LogDNA UI, which is all I really want.
+                // A 'prettiefied' meta object in LogDNA is kind of annoying read, I think.
+                prettyJson
+            ),
+            transports: [
+                new winston.transports.Console({
+                    level: process.env.NODE_ENV === 'test' ? 'error' : (process.env.LOG_LEVEL || 'info')
+                })
+            ]
+        });
 
-        global.logger = logger;
     }
 };
